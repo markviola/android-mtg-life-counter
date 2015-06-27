1698b3af25ca76ad6960634e8d7e540ac7327c90
Mark Anthony Viola <mark.anthony.viola@gmail.com>
2015-06-27 17:40:25 -0400

    Completed Poison Counter Fragments
    Completed the Java files for each of the poison counter fragments

---
+++ app/src/main/java/com/example/tony/mtglifecounter/ThreePlayerPoisonFragment.java
@@ -0,0 +1,64 @@
+package com.example.tony.mtglifecounter;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.util.Log;
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ThreePlayerPoisonFragment extends Fragment{
+
+    private static final String TAG = "Tony message";
+    Button add_life_total, sub_life_total;
+    TextView player_name, player_life;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.three_player_poison_fragment, container, false);
+        add_life_total = (Button) view.findViewById(R.id.add_life_total);
+        sub_life_total = (Button) view.findViewById(R.id.sub_life_total);
+        player_name = (TextView) view.findViewById(R.id.player_name);
+        player_life = (TextView) view.findViewById(R.id.player_life);
+
+        add_life_total.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View v){
+                        add_action();
+                    }
+                }
+        );
+
+        sub_life_total.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View v){
+                        sub_action();
+                    }
+                }
+        );
+
+        return view;
+    }
+
+    public void add_action(){
+        int current_life = Integer.parseInt(player_life.getText().toString());
+        if (current_life < 10) {
+            current_life++;
+            player_life.setText(String.valueOf(current_life));
+        }
+    }
+
+    public void sub_action(){
+        int current_life = Integer.parseInt(player_life.getText().toString());
+        if (current_life > 0) {
+            current_life--;
+            player_life.setText(String.valueOf(current_life));
+        }
+    }
+}
